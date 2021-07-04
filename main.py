403596334764b8542216c94a7fd6cc5bc1c2f9db
Derek Kwan <derek.x.kwan@gmail.com>
2021-07-04 03:31:47 -0700

    cleaner async

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from pythonosc.dispatcher import Dispatcher
 from aiohttp import web
 import asyncio
 import os
-from signal import signal, SIGINT
+import signal
+import functools
 from sys import exit
 
 script_path = os.path.abspath(os.path.dirname(__file__))
@@ -19,11 +20,10 @@ webport = int(os.getenv("PORT", 8080))
 ws = None
 to_send = []
 to_print = []
-running = True
 
-def sigint_handler(recv, frame):
-    global running
-    running = False
+def hard_exit(signame, loop):
+    loop.stop()
+    exit(0)
 
 def osc_to_ws(addr, *args):
     global ws
@@ -46,18 +46,17 @@ async def ws_handler(req):
     global ws
     ws = web.WebSocketResponse()
     await ws.prepare(req)
-    while running == True:
+    while True:
         while len(to_send) > 0:
             cur = to_send.pop()
             await ws.send_str(cur)
         await asyncio.sleep(WS_DUR)
     return ws
 
-async def loop():
+async def send_loop():
     global ws
     global to_send
-    global running
-    while running==True:
+    while True:
         for x in to_print:
             print(x)
             to_print.remove(x)
@@ -76,15 +75,18 @@ async def web_server():
     while True:
         await asyncio.sleep(3600)
 
-async def init_main():
+async def osc_server():
     server = AsyncIOOSCUDPServer((oscip, oscp), dispatcher, asyncio.get_event_loop())
     tport, protocol = await server.create_serve_endpoint()
-    await loop()
+    await send_loop()
     tport.close()
-    exit(0)
 
-signal(SIGINT, sigint_handler)
-asyncio.ensure_future(web_server())
-asyncio.ensure_future(init_main())
-mloop = asyncio.get_event_loop()
-mloop.run_forever()
+async def main():
+    loop = asyncio.get_event_loop()
+    for signame in {'SIGINT', 'SIGTERM'}:
+        loop.add_signal_handler(
+                getattr(signal, signame),
+                functools.partial(hard_exit, signame, loop))
+    await asyncio.gather(osc_server(), web_server())
+
+asyncio.run(main())
